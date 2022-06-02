362da0e5bc4edf559cb35c95a744459d9e44d179
Mykola Tydir <vputth1@gmail.com>
2022-06-02 17:01:16 +0300

    Add components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,28 @@
+import Card from "./components/Card"
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="wrapper clear">
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="logo" />
-          <div >
-            <h3 className="text-uppercase">
-              React Sneakers
-            </h3>
-            <p className="opacity-5">
-              Магазин лучших кроссовок
-            </p>
-          </div>
-        </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
-            <span>1205 р.</span>
-          </li>
-          <li>
-          <img width={18} height={18} src="/img/user.svg" alt="user" />
-          </li>
-        </ul>
-      </header>
+
+
+      <Drawer/>
+
+      <Header/>
+
 
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1 >Все кроссовки</h1>
+          <div className="search-block d-flex align-center">
+            <img width={18} height={18} src="/img/search.svg" alt="search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
 
         <div className="d-flex">
-          <div className="card">
-            <img width={133} height={122} src="/img/sneakers/1.jpg" alt="img" />
-            <h5>Мужские кроссовки Nike</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>
-                  Цена:
-                </span>
-                <b>
-                  12 999р.
-                </b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="plus" />
-              </button>
-              
-            </div>
-          </div>
+          <Card/>
         </div>
       </div>
 
